046e9af76bbb3f9c5dac265df9f8b428257e2e2d
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-26 20:35:23 +0530

    [Mahesh]Add:Annotation Employee Example

---
+++ Annotation/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annotation
+{
+    public class Employee
+    {
+      [Required(ErrorMessage = "Employee Name is required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name should be minimum 3 characters and a maximum of 100 characters")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+      [Required(ErrorMessage = "Employee Age is required")]
+        [Range(18, 99, ErrorMessage = "Age should be above 18")]
+        public int Age { get; set; }
+      [Required(ErrorMessage = "Employee Phone is required")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+      [Required(ErrorMessage = "Employee Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
--- Annotation/Program.cs
+++ Annotation/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annotation
+{
+    class Program
+    {
+        public static void Display()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("********Employee class Validation*********");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Employee objEmployee = new Employee();
+            objEmployee.Name = "Mahesh";
+            objEmployee.Age = 23;
+            objEmployee.PhoneNumber = "7829303710";
+            objEmployee.Email = "maheshhonnamurthy@gmail.com";
+
+            ValidationContext context = new ValidationContext(objEmployee, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(objEmployee, context, results, true);
+
+            if (!valid)
+            {
+                foreach (ValidationResult Totalresult in results)
+                {
+                    Console.WriteLine("Member Name : {0}", Totalresult.MemberNames.First(), Environment.NewLine);
+                    Console.WriteLine("Error Msg : {0}{1}", Totalresult.ErrorMessage, Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name: " + objEmployee.Name + "\n" + "Age: " + objEmployee.Age + "\n" + "PhoneNumber: " + objEmployee.PhoneNumber + "\n" + "Email: " + objEmployee.Email);
+            }
+            Console.ReadKey();
+        }
+        public static void Main(string[] args)
+        {
+            Program.Display();
+        }
+    }
+}
